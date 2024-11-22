6f0112e9da17e1428dfc2289019288bc37531964
nakashimashin <nakashimashin0517@gmail.com>
2024-11-22 23:49:08 +0900

    add: 必要なライブラリをインストール

---
+++ titanic_1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline,make_pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectKBest
+from sklearn import model_selection
+from sklearn.model_selection import GridSearchCV
+import warnings
+# warnings.filterwarnings('ignore')
